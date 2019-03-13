af0eb17d63f680ef45d331bc4ad4b5166003f14a
AiSDPM <48443641+AiSDPM@users.noreply.github.com>
2019-03-13 19:07:16 +0100

    Update rosnace.py

--- rosnace.py
+++ rosnace.py
@@ -87,6 +87,7 @@ lowerList = []
 heaList = []
 merList = []
 selList = []
+logfile = open('rosnace.txt', 'w')
 
 n = 1
 for j in range(15):
@@ -105,24 +106,27 @@ for j in range(15):
     InsertSort(insList)
     endTime = time.time()
     Time = endTime - startTime
-    print("InsertSort " + str(Time))
+     logfile.write("InsertSort " + str(Time)+ "\n")
 
     startTime = time.time()
     HeapSort(heaList)
     endTime = time.time()
     Time = endTime - startTime
-    print("HeapSort " + str(Time))
+    logfile.write("HeapSort " + str(Time)+ "\n")
 
     startTime = time.time()
     MergeSortMain(merList)
     endTime = time.time()
     Time = endTime - startTime
-    print("MergeSort " + str(Time))
+    logfile.write("MergeSort " + str(Time)+ "\n")
 
     startTime = time.time()
     SelectionSort(selList)
     endTime = time.time()
     Time = endTime - startTime
-    print("SelectionSort " + str(Time))
-    print(str(n) + ".    ----------------------------------------------------------------------------------------------------------")
+    logfile.write("SelectionSort " + str(Time) + "\n")
+    print(str(
+        n) + ".    ----------------------------------------------------------------------------------------------------------")
     n += 1
+logfile.close()
+
